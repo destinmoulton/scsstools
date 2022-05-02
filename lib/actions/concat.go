0b4609da53be9d06a38c1b0992a7e21f3192fec5
Destin Moulton <destin.moulton@gmail.com>
2022-05-01 18:59:58 -0600

    Added file checker

--- lib/actions/concat.go
+++ lib/actions/concat.go
@@ -1,6 +1,22 @@
 package actions
 
-// The concat action
-func Concat(sources []string, dest string) {
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
+// The concat action
+func Concat(action TAction) error {
+	sourceBase := action.SourcePath
+
+	for _, sourceFile := range action.Sources {
+		fp := filepath.Join(sourceBase, sourceFile)
+		if _, err := os.Stat(fp); errors.Is(err, os.ErrNotExist) {
+			return fmt.Errorf("concat: the file %s does not exist", fp)
+		}
+	}
+
+	return nil
 }
